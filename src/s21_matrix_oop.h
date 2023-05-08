3d668f0f638fe49145be0cc5349aef40aa30b9fb
MikhailPimenov <misha4git@yandex.ru>
2023-05-08 22:44:17 +0300

    develop/comments in header file

--- src/s21_matrix_oop.h
+++ src/s21_matrix_oop.h
@@ -9,25 +9,41 @@ class S21Matrix {
     private:
         // Attributes
         int rows_ = 0;
-        int columns_ = 0;                   // Rows and columns
-        double **matrix_ = nullptr;         // Pointer to the memory where the matrix is allocated
-
+        int columns_ = 0;
+        double **matrix_ = nullptr;         
     public:
 
         S21Matrix() noexcept;               // Default constructor
         S21Matrix(int rows, int columns);
         explicit S21Matrix(int dimension);  // Constructor for square matrix
+        
+        /*
+        To create matrix like that:
+        S21Matrix m({
+            {1.0,  2.0,  3.0,  4.0},
+            {5.0,  6.0,  7.0,  8.0},
+            {9.0, 10.0, 11.0, 12.0},
+        });
+        */
         explicit S21Matrix(const List& list);
 
         // rule of five
         ~S21Matrix();                       // Destructor
-        // TODO: copy-swap idiom
+        // TODO: implement copy-swap idiom
         S21Matrix(const S21Matrix& other);
         S21Matrix(S21Matrix&& other);
         S21Matrix& operator=(const S21Matrix& other);
         S21Matrix& operator=(S21Matrix&& other) noexcept;
         
 
+        /*
+        To modify matrix like that:
+        m = {
+            {1.0,  2.0,  3.0,  4.0},
+            {5.0,  6.0,  7.0,  8.0},
+            {9.0, 10.0, 11.0, 12.0},
+        };
+        */
         S21Matrix& operator=(const List& list);
 
 
@@ -51,6 +67,7 @@ class S21Matrix {
 
 
 
+        // operators
         friend S21Matrix operator+(const S21Matrix& left, const S21Matrix& right);
         friend S21Matrix operator-(const S21Matrix& left, const S21Matrix& right);
         friend S21Matrix operator*(const S21Matrix& left, const S21Matrix& right);
